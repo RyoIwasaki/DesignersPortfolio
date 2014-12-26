f9a67a6db06bccd03c774f21ea5b0b0485537de8
unknown <A13447@Sumzap-152.cyberagent.co.jp>
2014-12-26 23:02:20 +0900

    json add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,22 @@
 
 $(".search").on("click", function(){
 	console.log($(this));
-})
+});
+
+
+$(".header_title a").on("click", function(){
+	$.ajax({
+		type: 'GET',
+		cache: false,
+		url: 'data.json',
+		datatype: 'json',
+		success: function(data){
+			$.each(data, function(i, item){
+				console.log(item.name);
+			});
+		},
+		error: function(){
+			console.log("!!!!!!!!!!ajax error!!!!!!!!!!");
+		}
+	});
+});
--- app/scripts/orijinal.js
+++ app/scripts/orijinal.js
@@ -7,6 +7,24 @@
 
 $(".search").on("click", function(){
 	console.log($(this));
-})
+});
+
+
+$(".header_title a").on("click", function(){
+	$.ajax({
+		type: 'GET',
+		cache: false,
+		url: 'data.json',
+		datatype: 'json',
+		success: function(data){
+			$.each(data, function(i, item){
+				console.log(item.name);
+			});
+		},
+		error: function(){
+			console.log("!!!!!!!!!!ajax error!!!!!!!!!!");
+		}
+	});
+});
 
 })( window, document );
